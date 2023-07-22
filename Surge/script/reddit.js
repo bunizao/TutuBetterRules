03468ddb6c41f9b3a90483458d15052fe8e583e3
tutuis_me <102936102+bunizao@users.noreply.github.com>
2023-07-22 21:43:24 +0800

    Create reddit.js

---
+++ Surge/script/reddit.js
@@ -0,0 +1,15 @@
+const body = JSON.parse($response.body)
+let modified
+if (body.data) {
+  modified = true
+  if (body.data.subredditInfoByName && body.data.subredditInfoByName.elements && body.data.subredditInfoByName.elements.edges) {
+    body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
+  } else if (body.data.home && body.data.home.elements && body.data.home.elements.edges) {
+    body.data.home.elements.edges = body.data.home.elements.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
+  } else if(body.data.subredditsInfoByNames){
+    body.data.subredditsInfoByNames = body.data.subredditsInfoByNames.map(i => ({...i, isNsfw: false}))
+  } else {
+    modified = false
+  }
+}
+$done(modified ? { body: JSON.stringify(body) } : {})
