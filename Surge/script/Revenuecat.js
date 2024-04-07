0fadaa18651d9589db6222005e44dfa6ce9db8fe
tutuis_me <102936102+bunizao@users.noreply.github.com>
2024-04-07 13:06:35 +0800

    Update Revenuecat.js

--- Surge/script/Revenuecat.js
+++ Surge/script/Revenuecat.js
@@ -125,216 +125,145 @@ https://apps.apple.com/cn/app/id1531594277
 https://apps.apple.com/cn/app/id1120099014
 
 
-[rewrite_local]
 
-# ～ RevenueCat@ddgksf2013
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
-^https:\/\/api\.revenuecat\.com\/.+\/subscribers\/[^/]+/(offerings|attributes)$ url request-header (\r\n)X-RevenueCat-ETag:.+(\r\n) request-header $1X-RevenueCat-ETag:$2
+// ========= 动态ID ========= //
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'FinancialNote': ['category'],
+  'Precious/': ['Pro'],
+  'LUTCamera': ['ProVersion', 'com.uzero.funforcam.monthlysub'],
+  'totowallet': ['all', 'com.ziheng.totowallet.yearly'],
+  'Aphrodite': ['all'],
+  'apollo': ['all'],
+  'widget_art': ['all'],
+  'OneBox': ['all'],
+  'Spark': ['premium'],
+  'Pillow': ['premium'],
+  '1Blocker': ['premium'],
+  'VSCO': ['membership'],
+  'UTC': ['Entitlement.Pro'],
+  '%E8%AC%8E%E5%BA%95%E9%BB%91%E8%86%A0': ['Entitlement.Pro'],
+  '%E8%AC%8E%E5%BA%95%E6%99%82%E9%90%98': ['Entitlement.Pro'],
+  'OffScreen': ['Entitlement.Pro'],
+  'ScannerPro': ['plus'],
+  'WhiteCloud': ['allaccess'],
+  'HTTPBot': ['pro'],
+  'audiomack': ['Premium1'],
+  'server_bee': ['Pro'],
+  'simple-': ['patron'],
+  'streaks': ['patron'],
+  'andyworks-calculator': ['patron'],
+  'vibes': ['patron'],
+  'CountDuck': ['premium', 'Lifetime'],
+  'IPTVUltra': ['premium'],
+  'Happy%3ADays': ['pro', 'happy_999_lifetime'],
+  'PDF_convertor/': ['VIP', 'com.pdf.convertor.forever'],
+  'ChatGPTApp': ['Advanced'],
+  'APTV': ['pro'],
+  'TouchRetouchBasic': ['premium'],
+  'My%20Jump%20Lab': ['lifetime'],
+  '%E7%9B%AE%E6%A0%87%E5%9C%B0%E5%9B%BE': ['pro'],
+  'Paku': ['pro'],
+  'Awesome%20Habits': ['premium'],
+  'Gear': ['pro', 'com.gear.app.yearly'],
+  'MoneyThings': ['Premium'],
+  'Anybox': ['pro'],
+  'Fileball': ['filebox_pro'],
+  'Noto': ['pro'],
+  'Grow': ['grow.pro', 'grow_lifetime'],
+  'WidgetSmith': ['Premium'],
+  'Percento': ['premium'],
+  'Planny': ['premium'],
+  'CPUMonitor': ['Pro'],
+  'Locket': ['Gold'],
+  'My%20Tim': ['Pro'],
+  'Photom': ['premium', 'pixelmator_photo_pro_subscription_v1_pro_offer'],
+  'mizframa': ['premium', 'mf_20_lifetime2'],
+  'YzyFit/': ['pro', 'yzyfit_lft_v2'],
+  'ImageX': ['imagex.pro.ios', 'imagex.pro.ios.lifetime'],
+  'Fin': ['premium', 'com.circles.fin.premium.yearly'],
+  'Ledger': ['Pro', 'com.lifetime.pro'],
+  'One4Wall': ['lifetime', 'lifetime_key'],
+  'PhotoMark/': ['Pro', 'com.photo.mark.forever'],
+  'SimpleScan/': ['premium', 'com.atlantia.SimpleScan.Purchases.Lifetime'],
+  'OneWidget': ['allaccess'],
+  'CardPhoto': ['premium'],
+  'Journal_iOS/': ['PRO'],
+  'LemonKeepAccounts/': ['VIP','lm_1_1month'],
+  'PDF%20Viewer': ['sub.pro'],
+  'PhotoRoom': ['business'],
+  'Decision': ['com.nixwang.decision.entitlements.pro'],
+  'Tangerine': ['Premium'],
+  'PastePal': ['premium'],
+  'Fiery': ['premium'],
+  'Airmail': ['Airmail Premium'],
+  'Stress': ['StressWatch Pro'],
+  'PinPaper': ['allaccess'],
+  'Echo': ['PLUS'],
+  'MyThings': ['pro','xyz.jiaolong.MyThings.pro.infinity'],
+  'Overdue': ['Pro'],
+  'BlackBox': ['plus','app.filmnoir.appstore.purchases.lifetime'],
+  'Spektr': ['premium'],
+  'MusicMate': ['premium','mm_lifetime_68_premium'],
+  '%E4%BA%8B%E7%BA%BF': ['pro','xyz.jiaolong.eventline.pro.lifetime'],
+  'Tasks': ['Pro'],
+  'Currency': ['plus'],
+  'money_manager': ['premium'],
+  'fastdiet': ['premium'],
+  'Blurer': ['paid_access'],
+  'Everlog': ['premium'],
+  'reader': ['vip2','com.valo.reader.vip2.year'],
+  'GetFace': ['Pro access'],
+  'intervalFlow': ['All Access','wodtimer_lf_free'],
+  'Period%20Calendar': ['Premium','com.lbrc.PeriodCalendar.premium.yearly'],
+  'Cookie': ['allaccess','app.ft.Bookkeeping.lifetime'],
+  'ScientificCalculator': ['premium','com.simpleinnovation.calculator.ai.premium.yearly.base'],
+  'MOZE': ['premium'],
+  '1LemonKeepAccounts/': ['vip'],
+  'To%20Me/': ['Premium'],
+  '%E8%A8%80%E5%A4%96%E7%AD%86%E8%A8%98/': ['Premium'],
+  'alcohol.tracker': ['pro','drinklog_lifetime'],
+  'DayPoem': ['Pro Lifetime'],
+  'Budget%20Flow': ['full_access','com.fabian.hasse.haushaltsbuch.upgrade.combined'],
+  'G%20E%20I%20S%20T': ['memorado_premium'],
+  'multitimer_app': ['premium','timus_lt'],
+  'Darkroom': ['co.bergen.Darkroom.entitlement.allToolsAndFilters'],
+  'tiimo': ['full_access'],
+  'FaceMa/': ['Pro access'],
+  'Record2Text/': ['Pro access'],
+  'jinduoduo_calculator': ['jinduoduoapp','mobile_vip'],
+  'Focused%20Work': ['Pro'],
+  'GoToSleep': ['Pro'],
+  'kegel': ['kegel_pro'],
+  'Ochi': ['Pro'],
+  'Pomodoro': ['Plus','com.MINE.PomodoroTimer.plus.yearly'],
+  'universal/': ['Premium','remotetv.yearly.07'],
+  'ShellBean/': ['pro','com.ningle.shellbean.subscription.year'],
+  'AI%20Art%20Generator/': ['Unlimited Access'],
+  'Email%20Me': ['premium'],
+  'GoodThing/': ['pro','goodhappens_basic_year'],
+  'Reels%20Editor': ['Unlimited Access'],
+  'com.dison.diary': ['vip'],
+  'iRead': ['vip'],
+  'jizhi': ['jizhi_vip'],
+  'card/': ['vip'],
+  'EraseIt/': ['ProVersionLifeTime'],
+  'Alpenglow': ['newPro'],
+  'MindBreathYoga/': ['lifetimeusa'],
+  'MetadataEditor': ['unlimited_access'],
+  '%E6%9F%A5%E5%A6%86%E5%A6%86': ['Pro access'],
+  '%E5%85%83%E6%B0%94%E8%AE%A1%E6%97%B6': ['plus'],
+  'WidgetCat': ['MiaoWidgetPro'],
+  'Emphasis/': ['premium'],
+  'FormScanner/': ['Pro','formscanner_lifetime'],
+  'streamer/': ['Premium'],
+  'NeatNook/': ['com.neatnook.pro','com.neatnook.pro.forever'],
+  'Blackout/': ['premium','blackout_299_lt'],
+  'Budgetify/': ['premium','budgetify_3999_lt'],
+  'Dedupe/': ['Pro','com.curiouscreatorsco.Dedupe.pro.lifetime.notrial.39_99'],
+  'Wozi': ['wozi_pro_2023']
+};
 
-[mitm]
-
-hostname=api.revenuecat.com
-
-***********************************/
-
-
-
-
-var ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
-var cuttlefish = {"Attention":"恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！","request_date_ms":1662599120248,"request_date":"2022-09-08T01:05:20Z","subscriber":{"non_subscriptions":{},"first_seen":"2022-09-08T01:04:03Z","original_application_version":"8","other_purchases":{},"management_url":"https://apps.apple.com/account/subscriptions","subscriptions":{},"entitlements":{},"original_purchase_date":"2022-09-07T13:05:43Z","original_app_user_id":"$RCAnonymousID:ddgksf2013","last_seen":"2022-09-08T01:04:03Z"}};
-var ddgksf2013={"is_sandbox":false,"ownership_type":"PURCHASED","billing_issues_detected_at":null,"period_type":"normal","expires_date":"2099-12-18T01:04:17Z","grace_period_expires_date":null,"unsubscribe_detected_at":null,"original_purchase_date":"2022-09-08T01:04:18Z","purchase_date":"2022-09-08T01:04:17Z","store":"app_store"};
-var ddgksf2021={"grace_period_expires_date":null,"purchase_date":"2022-09-08T01:04:17Z","product_identifier":"ddgksf2013_1y_128","expires_date":"2099-12-18T01:04:17Z"};
-var obj = JSON.parse(JSON.stringify(cuttlefish));
-ddgksf2021['product_identifier']="com.ddgksf2013.premium.yearly";
-obj['subscriber']['subscriptions']['com.ddgksf2013.premium.yearly']=ddgksf2013;
-
-//动态ID分配
-if(ua.indexOf('%E8%BD%A6%E7%A5%A8%E7%A5%A8') != -1) {//车票票
- obj['subscriber']['entitlements']['vip+watch_vip']=ddgksf2021;
-}
-else if(ua.indexOf('LUTCamera') != -1) {//方弗相机
-	ddgksf2021['product_identifier']="com.uzero.funforcam.monthlysub";
-	obj['subscriber']['entitlements']['ProVersion']=ddgksf2021;
-	obj['subscriber']['subscriptions']['com.uzero.funforcam.monthlysub']=ddgksf2013;
-}
-else if(ua.indexOf('totowallet') != -1) {//图图记账
-	ddgksf2021['product_identifier']="com.ziheng.totowallet.yearly";
-	obj['subscriber']['entitlements']['all']=ddgksf2021;
-	obj['subscriber']['subscriptions']['com.ziheng.totowallet.yearly']=ddgksf2013;
-}
-else if(ua.indexOf('Aphrodite') != -1) {//Aphrodite
-	obj['subscriber']['entitlements']['all']=ddgksf2021;
-}
-else if(ua.indexOf('apollo') != -1) {//apollo
-	obj['subscriber']['entitlements']['all']=ddgksf2021;
-}
-else if(ua.indexOf('widget_art') != -1) {//widget_art
-	obj['subscriber']['entitlements']['all']=ddgksf2021;	
-}
-else if(ua.indexOf('OneBox') != -1) {//pandora
-  obj['subscriber']['entitlements']['all']=ddgksf2021;
-}
-else if(ua.indexOf('Spark') != -1) {//spark
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;	
-}
-else if(ua.indexOf('Pillow') != -1) {//pillow
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;
-}
-else if(ua.indexOf('1Blocker') != -1) {//1Blocker
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;
-}
-else if(ua.indexOf('VSCO') != -1) {//VSCO
-	obj['subscriber']['entitlements']['membership']=ddgksf2021;
-}
-else if(ua.indexOf('UTC') != -1) {//花样文字
-	obj['subscriber']['entitlements']['Entitlement.Pro']=ddgksf2021;	
-}
-else if(ua.indexOf('%E8%AC%8E%E5%BA%95%E9%BB%91%E8%86%A0') != -1) {//谜底黑胶
-	obj['subscriber']['entitlements']['Entitlement.Pro']=ddgksf2021;	
-}
-else if(ua.indexOf('%E8%AC%8E%E5%BA%95%E6%99%82%E9%90%98') != -1) {//谜底时钟
-	obj['subscriber']['entitlements']['Entitlement.Pro']=ddgksf2021;
-}
-else if(ua.indexOf('OffScreen') != -1) {//OffScreen
-	obj['subscriber']['entitlements']['Entitlement.Pro']=ddgksf2021;	
-}
-else if(ua.indexOf('ScannerPro') != -1) {//Scanner Pro
-	obj['subscriber']['entitlements']['plus']=ddgksf2021;
-}
-else if(ua.indexOf('WhiteCloud') != -1) {//WhiteCloud
-	obj['subscriber']['entitlements']['allaccess']=ddgksf2021;
-}
-else if(ua.indexOf('HTTPBot') != -1) {//httpbot
-  obj['subscriber']['entitlements']['pro']=ddgksf2021;
-}
-else if(ua.indexOf('audiomack') != -1) {//Audiomack
-	obj['subscriber']['entitlements']['Premium1']=ddgksf2021;
-}
-else if(ua.indexOf('server_bee') != -1) {//server_bee
-	obj['subscriber']['entitlements']['Pro']=ddgksf2021;
-}
-else if(ua.indexOf('simple-') != -1) {//NotBoring
-	obj['subscriber']['entitlements']['patron']=ddgksf2021;
-}
-else if(ua.indexOf('streaks') != -1) {//习惯
-	obj['subscriber']['entitlements']['patron']=ddgksf2021;
-}
-else if(ua.indexOf('andyworks-calculator') != -1) {//计算器
-	obj['subscriber']['entitlements']['patron']=ddgksf2021;
-}
-else if(ua.indexOf('vibes') != -1) {//vibes
-	obj['subscriber']['entitlements']['patron']=ddgksf2021;
-}
-else if(ua.indexOf('CountDuck') != -1) {//vibes
-	ddgksf2021['product_identifier']="Lifetime";
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;
-	obj['subscriber']['subscriptions']['Lifetime']=ddgksf2013;
-}
-else if(ua.indexOf('IPTVUltra') != -1) {//vibes
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;
-}
-else if(ua.indexOf('Happy%3ADays') != -1) {//HappyDays
-	ddgksf2021['product_identifier']="happy_999_lifetime";
-	obj['subscriber']['entitlements']['pro']=ddgksf2021;
-	obj['subscriber']['subscriptions']['happy_999_lifetime']=ddgksf2013;
-}
-else if(ua.indexOf('ChatGPTApp') != -1) {//chat-ai-ask-chatbot-assistant
-	obj['subscriber']['entitlements']['Advanced']=ddgksf2021;
-}
-else if(ua.indexOf('APTV') != -1) {//APTV
-	obj['subscriber']['entitlements']['pro']=ddgksf2021;	
-}
-else if(ua.indexOf('TouchRetouchBasic') != -1) {//TouchRetouchBasic
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;	
-}
-else if(ua.indexOf('My%20Jump%20Lab') != -1) {//My%20Jump%20Lab
-	obj['subscriber']['entitlements']['lifetime']=ddgksf2021;	
-}
-else if(ua.indexOf('%E7%9B%AE%E6%A0%87%E5%9C%B0%E5%9B%BE') != -1) {//目标地图
-	obj['subscriber']['entitlements']['pro']=ddgksf2021;
-}
-else if(ua.indexOf('Paku') != -1) {//Paku
-  obj['subscriber']['entitlements']['pro']=ddgksf2021;
-}
-else if(ua.indexOf('Awesome%20Habits') != -1) {//Awesome%20Habits
-  obj['subscriber']['entitlements']['premium']=ddgksf2021;
-}
-else if(ua.indexOf('Gear') != -1) {//Gear	
-	ddgksf2021['product_identifier']="com.gear.app.yearly";
-	obj['subscriber']['entitlements']['pro']=ddgksf2021;
-	obj['subscriber']['subscriptions']['com.gear.app.yearly']=ddgksf2013;
-
-}
-else if(ua.indexOf('MoneyThings') != -1) {//MoneyThings
-	obj['subscriber']['entitlements']['Premium']=ddgksf2021;
-}
-else if(ua.indexOf('Anybox') != -1) {//Anybox
-	obj['subscriber']['entitlements']['pro']=ddgksf2021;	
-}
-else if(ua.indexOf('Fileball') != -1) {//Filebox
-	obj['subscriber']['entitlements']['filebox_pro']=ddgksf2021;
-}
-else if(ua.indexOf('Noto') != -1) {//noto
-  obj['subscriber']['entitlements']['pro']=ddgksf2021;
-}
-else if(ua.indexOf('Grow') != -1) {
-	ddgksf2021['product_identifier']="grow_lifetime";
-	obj['subscriber']['entitlements']['grow.pro']=ddgksf2021;
-	obj['subscriber']['subscriptions']['grow_lifetime']=ddgksf2013;
-}
-else if(ua.indexOf('WidgetSmith') != -1) {//Widgetsmith
-	obj['subscriber']['entitlements']['Premium']=ddgksf2021;
-}
-else if(ua.indexOf('Percento') != -1) {//Percento
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;	
-}
-else if(ua.indexOf('Planny') != -1) {//Planny 6
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;
-}
-else if(ua.indexOf('CPUMonitor') != -1) {//手机硬件管家
-	obj['subscriber']['entitlements']['Pro']=ddgksf2021;	
-}
-else if(ua.indexOf('Locket') != -1) {
-	obj['subscriber']['entitlements']['Gold']=ddgksf2021;	
-}
-else if(ua.indexOf('My%20Tim') != -1) {
-	obj['subscriber']['entitlements']['Pro']=ddgksf2021;	
-}
-else if(ua.indexOf('Photom') != -1) {
-	ddgksf2021['product_identifier']="pixelmator_photo_pro_subscription_v1_pro_offer";
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;
-	obj['subscriber']['subscriptions']['pixelmator_photo_pro_subscription_v1_pro_offer']=ddgksf2013;	
-}
-else if(ua.indexOf('mizframa') != -1) {
-	ddgksf2021['product_identifier']="mf_20_lifetime2";
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;
-	obj['subscriber']['subscriptions']['mf_20_lifetime2']=ddgksf2013;
-}
-else if(ua.indexOf('ImageX') != -1) {
-	ddgksf2021['product_identifier']="imagex.pro.ios.lifetime";
-	obj['subscriber']['entitlements']['imagex.pro.ios']=ddgksf2021;
-	obj['subscriber']['subscriptions']['imagex.pro.ios.lifetime']=ddgksf2013;	
-}
-else if(ua.indexOf('Fin') != -1) {//Fin
-	ddgksf2021['product_identifier']="com.circles.fin.premium.yearly";
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;
-	obj['subscriber']['subscriptions']['com.circles.fin.premium.yearly']=ddgksf2013;
-}
-else if(ua.indexOf('One4Wall') != -1) {
-	ddgksf2021['product_identifier']="lifetime_key";
-	obj['subscriber']['entitlements']['lifetime']=ddgksf2021;
-	obj['subscriber']['subscriptions']['lifetime_key']=ddgksf2013;
-}
-else if(ua.indexOf('OneWidget') != -1) {
-	obj['subscriber']['entitlements']['allaccess']=ddgksf2021;
-}
-else if(ua.indexOf('CardPhoto') != -1) {
-	obj['subscriber']['entitlements']['premium']=ddgksf2021;	
-}
-else if(ua.indexOf('PDF%20Viewer') != -1) {
-	obj['subscriber']['entitlements']['sub.pro']=ddgksf2021;	
-}
-else{
-  obj['subscriber']['entitlements']['pro']=ddgksf2021;
-}
-$done({body: JSON.stringify(obj)});
+// =========    固定部分  ========= // 
+// =========  @ddgksf2021 ========= // 
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！";var ddgksf2013={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2022-09-08T01:04:18Z",purchase_date:"2022-09-08T01:04:17Z",store:"app_store"},ddgksf2021={grace_period_expires_date:null,purchase_date:"2022-09-08T01:04:17Z",product_identifier:"com.ddgksf2013.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(ddgksf2021.product_identifier=s,obj.subscriber.subscriptions[s]=ddgksf2013):obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"]=ddgksf2013,obj.subscriber.entitlements[e]=ddgksf2021}else obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"]=ddgksf2013,obj.subscriber.entitlements.pro=ddgksf2021;$done({body:JSON.stringify(obj)});
